8c842a3cf95c3b264bb9a22baad86b0cb7154014
Eitan Elberg <eitan.elberg@consumrz.com>
2025-03-19 15:21:12 +0200

    Services section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Benefits from "./components/Benefits.jsx";
 import Collaboration from "./components/Collaboration.jsx";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
+import Services from "./components/Services.jsx";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
